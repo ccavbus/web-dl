4bef464febca32cadac3792021eb5874350e04c4
ghcdn <72659189+ghcdn@users.noreply.github.com>
2022-03-04 20:09:08 +0800

    rm delog

--- delog.py
+++
@@ -1,9 +0,0 @@
-from ghapi.all import GhApi
-api = GhApi()
-
-owner = 'ghcdn'
-repo = 'video-dl'
-
-runs = api.actions.list_workflow_runs_for_repo(owner, repo)
-for run in runs.workflow_runs:
-    api.actions.delete_workflow_run(owner, repo, run.id)
