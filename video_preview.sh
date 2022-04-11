a1e44ea12d3544ce1bedd9757587aad7889c858e
ghcdn <github@chan.im>
2022-04-11 19:57:35 +0800

    rm ffprobe

--- video_preview.sh
+++ video_preview.sh
@@ -35,29 +35,20 @@ OUT_FILEPATH=`echo $OUT_DIR/$OUT_FILENAME`
 
 TOTAL_IMAGES=`echo "$COLS*$ROWS" | bc`
 
-# get total number of frames in the video
-# ffprobe is fast but not 100% reliable. It might not detect number of frames correctly!
-NB_FRAMES=`ffprobe -show_streams "$MOVIE" 2> /dev/null | grep nb_frames | head -n1 | sed 's/.*=//'`
-# `-show-streams` Show all streams found in the video. Each video has usualy two streams (video and audio).
-# `head -n1` We care only about the video stream which comes first.
-# `sed 's/.*=//'` Grab everything after `=`.
-
-if [ "$NB_FRAMES" = "N/A" ]; then
-    # as a fallback we'll use ffmpeg. This command basically copies this video to /dev/null and it counts
-    # frames in the process. It's slower (few seconds usually) than ffprobe but works everytime.
-    NB_FRAMES=`ffmpeg -nostats -i "$MOVIE" -vcodec copy -f rawvideo -y /dev/null 2>&1 | grep frame | awk '{split($0,a,"fps")}END{print a[1]}' | sed 's/.*= *//'`
-    # I know, that `awk` and `sed` parts look crazy but it has to be like this because ffmpeg can
-    # `-nostats` By default, `ffmpeg` prints progress information but that would be immediately caught by `grep`
-    #     because it would contain word `frame` and therefore output of this entire command would be totally
-    #      random. `-nostats` forces `ffmpeg` to print just the final result.
-    # `-i "$MOVIE"` Input file
-    # `-vcodec copy -f rawvideo` We don't want to do any reformating. Force `ffmpeg` to read and write the video as is.
-    # `-y /dev/null` Dump read video data. We just want it to count frames we don't care about the data.
-    # `awk ...` The line we're interested in has format might look like `frame= 42` or `frame=325`. Because of that
-    #     extra space we can't just use `awk` to print the first column and we have to cut everything from the
-    #     beggining of the line to the term `fps` (eg. `frame= 152`).
-    # `sed ...` Grab everything after `=` and ignore any spaces
-fi
+# as a fallback we'll use ffmpeg. This command basically copies this video to /dev/null and it counts
+# frames in the process. It's slower (few seconds usually) than ffprobe but works everytime.
+NB_FRAMES=`ffmpeg -nostats -i "$MOVIE" -vcodec copy -f rawvideo -y /dev/null 2>&1 | grep frame | awk '{split($0,a,"fps")}END{print a[1]}' | sed 's/.*= *//'`
+# I know, that `awk` and `sed` parts look crazy but it has to be like this because ffmpeg can
+# `-nostats` By default, `ffmpeg` prints progress information but that would be immediately caught by `grep`
+#     because it would contain word `frame` and therefore output of this entire command would be totally
+#      random. `-nostats` forces `ffmpeg` to print just the final result.
+# `-i "$MOVIE"` Input file
+# `-vcodec copy -f rawvideo` We don't want to do any reformating. Force `ffmpeg` to read and write the video as is.
+# `-y /dev/null` Dump read video data. We just want it to count frames we don't care about the data.
+# `awk ...` The line we're interested in has format might look like `frame= 42` or `frame=325`. Because of that
+#     extra space we can't just use `awk` to print the first column and we have to cut everything from the
+#     beggining of the line to the term `fps` (eg. `frame= 152`).
+# `sed ...` Grab everything after `=` and ignore any spaces
 
 # calculate offset between two screenshots, drop the floating point part
 NTH_FRAME=`echo "$NB_FRAMES/$TOTAL_IMAGES" | bc`
