1d62772ecdc97e536302f7af8b51a8a3c91cdc77
ghcdn <github@chan.im>
2022-04-15 20:08:16 +0800

    Update add_tracker.py

--- aria2/add_tracker.py
+++ aria2/add_tracker.py
@@ -2,17 +2,20 @@
 import requests
 
 def add_tracker(mylist, url):
-    with requests.get(url) as r:
-        trackers = ','.join( mylist + r.text.split('\n\n') )
-    with open('./template.conf') as f:
-        template = f.read()
-    with open('./aria2.conf', 'w') as f:
-        f.write(template + f'bt-tracker={trackers}')
+    r = requests.get(url)
+    trackers = ','.join(mylist) + ',' +  r.text
+    with open('./aria2/aria2.conf', 'a+') as f:
+        f.write(f'bt-tracker={trackers}')
     print("tracker added!")
 
 
 if __name__ == '__main__':
     mylist = ["http://sukebei.tracker.wf:8888/announce",
               "http://tracker.bt4g.com:2095/announce"]
+<<<<<<< HEAD
     url = "https://ngosang.github.io/trackerslist/trackers_best.txt"
     add_tracker(mylist, url)
+=======
+    url = "https://trackerslist.com/best_aria2.txt"
+    add_tracker(mylist, url)
+>>>>>>> parent of a7c798a (Update add_tracker.py)
