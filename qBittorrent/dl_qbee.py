058556a8d81f7887eb62daa27c428591c113c5b7
ghcdn <github@chan.im>
2022-04-16 15:32:22 +0800

    Update dl_qbee.py

--- qBittorrent/dl_qbee.py
+++ qBittorrent/dl_qbee.py
@@ -4,12 +4,13 @@ import subprocess
 def dl_latest(name):
     url = "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest"
     with requests.get(url) as r:
-        res = r.text.json()["assets"]
+        res = r.json()["assets"]
         for i in res:
             if i["name"] == name:
                 dlink = i["browser_download_url"]
     subprocess.run(["wget", dlink])
     subprocess.run(["unzip", name])
+    subprocess.run(["rm", name])
 
 if __name__ == "__main__":
     dl_latest("qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip")
